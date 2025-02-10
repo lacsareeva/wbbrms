7d7d85bdce2b5e037c84b3d83742cebdbeb747db
lacsareeva <164177749+lacsareeva@users.noreply.github.com>
2025-02-11 01:17:12 +0800

    official-content.blade.php

--- resources/views/admin/residentofficials/official-content.blade.php
+++ resources/views/admin/residentofficials/official-content.blade.php
@@ -7,8 +7,7 @@
     <div class="officials-container">
         @forelse($officialsinfo as $officialsinfos)
             <div class="official-card">
-                <img src="{{ asset('storage/' . $officialsinfos->officialsimage) }}" alt="{{ $officialsinfos->fullname }}"
-                    class="official-image">
+               <img src="{{ Storage::url($officialsinfos->officialsimage) }}" alt="Official's Image" class="official-image">
                 <p class="position">
                     <input style="color:#16a085" type="text" name="position" value="{{ $officialsinfos->position }}"
                         readonly><br>
