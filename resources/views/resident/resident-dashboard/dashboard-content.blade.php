11836a3f5af0b9c7dd3326dfec319615962e39a1
lacsareeva <164177749+lacsareeva@users.noreply.github.com>
2025-02-10 23:26:37 +0800

    dashboard-content.blade.php

--- resources/views/resident/resident-dashboard/dashboard-content.blade.php
+++ resources/views/resident/resident-dashboard/dashboard-content.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @vite(['resources/css/ResidentStyles/AnnouncementStyles.css'])
+    @vite(['resources/css/ResidentStyles/announcementStyles.css'])
 
 </head>
 
