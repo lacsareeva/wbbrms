73d12ded022ab8e377f120cc221a03d993c097eb
lacsareeva <164177749+lacsareeva@users.noreply.github.com>
2025-02-10 14:55:54 +0800

    AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,7 @@ use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Route;
 use App\Http\Middleware\EnsureCorrectGuard;
 use App\Http\Middleware\CheckUserRole;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
